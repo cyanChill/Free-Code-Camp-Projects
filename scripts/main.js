dbb72e1d5f7bff8f76dd9d64455a2e5837330bcf
cyanchill <anthonyliang9@gmail.com>
2021-08-03 16:14:17 -0400

    added timer countdown funcitonality & uninterrupted switch between session and break timers

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,125 @@
+const defaultSettings = {
+    breakLen: 5,
+    sessionLen: 25,
+    timing: {
+        status: 'paused',
+        min: 25,
+        sec: 0,
+        timeVar: '',
+        type: 'Session',
+    },
+};
+
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            breakLen: 5,
-            sessionLen: 25,
-        };
+        this.state = defaultSettings;
         this.playAlarm = this.playAlarm.bind(this);
         this.updateLen = this.updateLen.bind(this);
+        this.runTimer = this.runTimer.bind(this);
+        this.resetTimer = this.resetTimer.bind(this);
+        this.countDown = this.countDown.bind(this);
     }
 
     playAlarm() {
 
     }
 
+    // Returns interval function for countdown
+    countDown() {
+        return setInterval(() => {
+            let {timing} = this.state;
+            if (timing.sec > 0) {
+                this.setState({
+                    timing: {
+                        ...this.state.timing,
+                        sec: timing.sec - 1,
+                    },
+                });
+            } else if (timing.min > 0) {
+                this.setState({
+                    timing: {
+                        ...this.state.timing,
+                        min: timing.min - 1,
+                        sec: 59,
+                    },
+                });
+            } else {
+                clearInterval(this.state.timing.timeVar);
+                this.setState({
+                    timing: {
+                        ...this.state.timing,
+                        min: timing.type === 'Session' ? this.state.breakLen : this.state.sessionLen,
+                        timeVar: this.countDown(),
+                        type: timing.type === 'Session' ? 'Break' : 'Session',
+                    },
+                });
+            }
+        }, 1000);
+    }
+
+    runTimer() {
+        let {timing} = this.state;
+
+        if (timing.status === 'play') {
+            clearInterval(timing.timeVar);
+        }
+        this.setState({
+            timing: {
+                ...this.state.timing,
+                status: timing.status === 'paused' ? 'play' : 'paused',
+                timeVar: timing.status === 'paused' ? this.countDown() : '',
+            },
+        });
+
+    }
+
+    resetTimer() {
+        clearInterval(this.state.timing.timeVar);
+        this.setState(defaultSettings);
+    }
+
     updateLen(event) {
         const type = event.target.id.split('-');
-        const {breakLen, sessionLen} = this.state;
-        if (type[0] === 'break') {
-            if (type[1] === 'increment' && breakLen !== 60) {
-                this.setState({ breakLen: breakLen + 1 });
-            } else if (type[1] === 'decrement' && breakLen !== 1){
-                this.setState({ breakLen: breakLen - 1 });
-            }
-        } else if (type[0] === 'session') {
-            if (type[1] === 'increment' && sessionLen !== 60) {
-                this.setState({ sessionLen: sessionLen + 1 });
-            } else if (type[1] === 'decrement' && sessionLen !== 1){
-                this.setState({ sessionLen: sessionLen - 1 });
+        const {breakLen, sessionLen, timing} = this.state;
+        if (timing.status === 'paused') {
+            this.setState({
+                timing: {
+                    ...this.state.timing,
+                    sec: 0,
+                    type: 'Session',
+                },
+            });
+
+            if (type[0] === 'break') {
+                if (type[1] === 'increment' && breakLen !== 60) {
+                    this.setState((state) => { return { breakLen: state.breakLen + 1 }});
+                } else if (type[1] === 'decrement' && breakLen !== 1){
+                    this.setState((state) => { return { breakLen: state.breakLen - 1 }});
+                }
+            } else if (type[0] === 'session') {
+                if (type[1] === 'increment' && sessionLen !== 60) {
+                    this.setState((state) => {
+                        return { 
+                            sessionLen: state.sessionLen + 1,
+                            timing: {
+                                ...state.timing,
+                                min: state.sessionLen + 1,
+                        }
+                    }});
+                } else if (type[1] === 'decrement' && sessionLen !== 1){
+                    this.setState((state) => {
+                        return { 
+                            sessionLen: state.sessionLen - 1,
+                            timing: {
+                                ...state.timing,
+                                min: state.sessionLen - 1,
+                        }
+                    }});
+                }
             }
         }
     }
-
-
-    // Try to use multiple components
 
 
     render() {
@@ -51,19 +138,21 @@ class App extends React.Component {
             );
         });
 
+        const {timing} = this.state;
+
 
         return (
             <div id="project-container">
                 {lengthControl}
                 <div id="timer-container">
-                    <p id="timer-label">Session</p>
-                    <p id="time-left">25:00</p>
+                    <p id="timer-label">{timing.type}</p>
+                    <p id="time-left">{`${(timing.min).toString().padStart(2, '0')}:${(timing.sec).toString().padStart(2, '0')}`}</p>
                     <div id="media-controls">
-                        <button id="start_stop">
+                        <button id="start_stop" onClick={this.runTimer}>
                             <i className="fas fa-play"></i>
                             <i className="fas fa-pause"></i>
                         </button>
-                        <button id="reset">
+                        <button id="reset" onClick={this.resetTimer}>
                             <i className="fas fa-sync-alt"></i>
                         </button>
                     </div>
